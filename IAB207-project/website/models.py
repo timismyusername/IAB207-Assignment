78e9251ead7f115833b4585bb3d5438e08d48893
kevtheuwyn <105707422+kevtheuwyn@users.noreply.github.com>
2022-06-12 18:37:39 +0800

    Adding user database

--- IAB207-project/website/models.py
+++ IAB207-project/website/models.py
@@ -2,6 +2,20 @@ from . import db
 from datetime import datetime
 from flask_login import UserMixin
 
+class User(db.Model, UserMixin):
+    __tablename__='users' 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+    contact_num = db.Column(db.INT(), index=True, unique=True, nullable=False)
+    address = db.Column(db.String(200), index=True, nullable=False)
+	#password is never stored in the DB, an encrypted password is stored
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    # relation to call user.comments and comment.created_by
+    comments = db.relationship('Comment', backref='user')
+
 
 
 class Event(db.Model):
