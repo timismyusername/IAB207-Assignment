d863f21e2569240d481a50f5dca33196a1a0eba9
kevtheuwyn <105707422+kevtheuwyn@users.noreply.github.com>
2022-06-12 18:36:58 +0800

    Added the register and login form

--- IAB207-project/website/forms.py
+++ IAB207-project/website/forms.py
@@ -7,7 +7,28 @@ from flask_wtf.file import FileRequired, FileField, FileAllowed
 ALLOWED_FILE = {'PNG', 'JPG', 'png', 'jpg', 'jpeg'}
 
 
+#creates the login information
+class LoginForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
+    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    submit = SubmitField("Login")
 
+ # this is the registration form
+class RegisterForm(FlaskForm):
+    user_name=StringField("User Name", validators=[InputRequired("Create a user name"), Length(min=5, max=20)])
+    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
+    phone_number = IntegerField("Contact number", validators=[InputRequired("Please enter a valid phone number")])
+    address = StringField("Address", validators=[InputRequired(), Length(min=5, max=30)])
+    #add buyer/seller - check if it is a buyer or seller hint : Use RequiredIf field
+
+
+    #linking two fields - password should be equal to data entered in confirm
+    password=PasswordField("Password", validators=[InputRequired(),
+                  EqualTo('confirm', message="Passwords should match")])
+    confirm = PasswordField("Confirm Password")
+
+    #submit button
+    submit = SubmitField("Register")
 
 #Create new event
 class EventForm(FlaskForm):
